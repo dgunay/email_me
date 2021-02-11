988337e9c55829bc115d65559460b3661a155395
Devin Gunay <devingunay@gmail.com>
2021-02-11 14:58:36 -0800

    wi

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,67 @@
-#[derive(Debug)]
-pub enum Error {
-    Generic(String),
-}
 
-impl From<serde_json::Error> for Error {
-    fn from(e: serde_json::Error) -> Self {
-        Error::Generic(e.to_string())
-    }
-}
 
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Error::Generic(e.to_string())
-    }
-}
+// #[derive(Debug)]
+// pub enum Error {
+//     Generic(String),
+// }
 
-impl From<rusoto_core::region::ParseRegionError> for Error {
-    fn from(e: rusoto_core::region::ParseRegionError) -> Self {
-        Error::Generic(e.to_string())
-    }
-}
+// impl std::fmt::Display for Error {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         todo!()
+//     }
+// }
 
-impl From<rusoto_core::credential::CredentialsError> for Error {
-    fn from(e: rusoto_core::credential::CredentialsError) -> Self {
-        Error::Generic(e.to_string())
-    }
-}
+// impl std::error::Error for Error {
+//     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+//         None
+//     }
 
-impl<T: std::error::Error + 'static> From<rusoto_core::RusotoError<T>> for Error {
-    fn from(e: rusoto_core::RusotoError<T>) -> Self {
-        Error::Generic(format!("{}", e))
-    }
-}
+//     // fn type_id(&self, _: private::Internal) -> std::any::TypeId
+//     // where
+//     //     Self: 'static,
+//     // {
+//     //     std::any::TypeId::of::<Self>()
+//     // }
+
+//     // fn backtrace(&self) -> Option<&std::backtrace::Backtrace> {
+//     //     None
+//     // }
+
+//     fn description(&self) -> &str {
+//         "description() is deprecated; use Display"
+//     }
+
+//     // fn cause(&self) -> Option<&dyn std::error::Error> {
+//     //     std::error.source()
+//     // }
+// }
+
+// impl From<serde_json::Error> for Error {
+//     fn from(e: serde_json::Error) -> Self {
+//         Error::Generic(e.to_string())
+//     }
+// }
+
+// impl From<std::io::Error> for Error {
+//     fn from(e: std::io::Error) -> Self {
+//         Error::Generic(e.to_string())
+//     }
+// }
+
+// impl From<rusoto_core::region::ParseRegionError> for Error {
+//     fn from(e: rusoto_core::region::ParseRegionError) -> Self {
+//         Error::Generic(e.to_string())
+//     }
+// }
+
+// impl From<rusoto_core::credential::CredentialsError> for Error {
+//     fn from(e: rusoto_core::credential::CredentialsError) -> Self {
+//         Error::Generic(e.to_string())
+//     }
+// }
+
+// impl<T: std::error::Error + 'static> From<rusoto_core::RusotoError<T>> for Error {
+//     fn from(e: rusoto_core::RusotoError<T>) -> Self {
+//         Error::Generic(format!("{}", e))
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 mod error;
 
-use error::Error;
+use futures::TryFutureExt;
 use getopts::Options;
+use hyper::{Body, Request, Response, Server, body::to_bytes, service::{make_service_fn, service_fn}};
 use rusoto_core::Region;
-use rusoto_sns::{PublishInput, Sns, SnsClient};
+use rusoto_sns::{PublishInput, PublishResponse, Sns, SnsClient};
 use serde::Deserialize;
-use std::{env, str::FromStr};
+use tokio::runtime::Runtime;
+use std::{convert::Infallible, env, net::SocketAddr, str::FromStr};
+use anyhow::Result;
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} <message>", program);
     print!("{}", opts.usage(&brief));
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
 
@@ -26,7 +29,7 @@ fn main() -> Result<(), Error> {
     opts.optopt("t", "topic-arn", "Topic ARN", "");
     opts.optopt("s", "subject", "Subject", "");
     opts.optflag("e", "server", "Serve requests continuously");
-    
+
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => panic!(f.to_string()),
@@ -43,54 +46,81 @@ fn main() -> Result<(), Error> {
         .unwrap_or("arn:aws:sns:us-east-2:250463611689:email-me".to_string());
 
     if matches.opt_present("e") {
-        serve_requests();
+        Runtime::new().unwrap().block_on(serve_requests());
     } else {
         let subject = matches.opt_str("s");
-    
+
         let message = if !matches.free.is_empty() {
             matches.free[0].clone()
         } else {
             print_usage(&program, opts);
             return Ok(());
         };
-    
-        publish_message(&client, message, subject, topic_arn)?;
+
+        // Unfortunately we need the entire futures/tokio jungle attached to our
+        // gorilla and banana in order to use rusoto, even if we only need to
+        // make a single blocking call.
+        let _resp = tokio::runtime::Runtime::new()
+            .expect("Failed to setup tokio runtime")
+            .block_on(publish_message(&client, message, subject, topic_arn))?;
     }
 
     Ok(())
 }
 
-fn serve_requests() -> ! {
-    loop {
-        todo!("serve requests")
+async fn serve_requests() -> Result<()> {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(service_publish_message))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
     }
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct Payload {
+    subject: Option<String>,
+    message: String,
+}
+
+async fn service_publish_message(req: Request<Body>) -> Result<Response<Body>> {
+    // Attempt to deserialize the body as json
+    let payload: Payload = serde_json::from_slice(req.body())?;
+
+    Ok(Response::new("Message sent.".into()))
 }
 
 // Send the message to our SNS topic
-fn publish_message(
+async fn publish_message(
     client: &SnsClient,
     message: String,
     subject: Option<String>,
     topic_arn: String,
-) -> Result<(), Error> {
+) -> Result<PublishResponse, rusoto::error::> {
     println!("Subject: {}", subject.clone().unwrap_or("".to_string()));
     println!("Message: {}", message);
 
-    // Unfortunately we need the entire futures/tokio jungle attached to our
-    // gorilla and banana in order to use rusoto.
-    let _response = tokio::runtime::Runtime::new()
-        .expect("Failed to setup tokio runtime")
-        .block_on(client.publish(PublishInput {
-            message,
-            message_attributes: None,
-            message_deduplication_id: None,
-            message_group_id: None,
-            message_structure: None,
-            phone_number: None,
-            subject,
-            target_arn: None,
-            topic_arn: Some(topic_arn),
-        }))?;
-    
-    Ok(())
+    client.publish(PublishInput {
+        message,
+        message_attributes: None,
+        message_deduplication_id: None,
+        message_group_id: None,
+        message_structure: None,
+        phone_number: None,
+        subject,
+        target_arn: None,
+        topic_arn: Some(topic_arn),
+    }).into().await
 }
